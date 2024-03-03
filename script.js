037349e11dda2ec30529748dc29c8391bbd05cc4
artemskakun <skakunaartem947@gmail.com>
2024-03-03 13:59:55 +0300

    Add destroy. Add BEM in index

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
 const initSlider = () => {
   const initCarousel = (containerId, images) => {
     const container = document.getElementById(containerId)
-    const imageList = container.querySelector('.image-list')
+    let imageList = container.querySelector('.image-list')
+    let maxScrollLeft
+
+    const destroyCarousel = () => {
+      imageList.removeEventListener('scroll', handleScroll)
+      window.removeEventListener('resize', updateSlider)
+      imageList = null
+      maxScrollLeft = null
+    }
+
+    const updateMaxScrollLeft = () => {
+      maxScrollLeft = imageList.scrollWidth - imageList.clientWidth
+    }
+
+    const updateSlider = () => {
+      updateMaxScrollLeft()
+      updateScrollThumbPosition()
+      handleSlideButtons()
+    }
+
+    const handleScroll = () => {
+      updateScrollThumbPosition()
+      handleSlideButtons()
+    }
+
+    const updateScrollThumbPosition = () => {
+      const scrollPosition = imageList.scrollLeft
+      const thumbPosition =
+        (scrollPosition / maxScrollLeft) *
+        (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth)
+      scrollbarThumb.style.left = `${thumbPosition}px`
+    }
+
+    const handleSlideButtons = () => {
+      slideButtons[0].style.display = imageList.scrollLeft <= 0 ? 'none' : 'flex'
+      slideButtons[1].style.display =
+        imageList.scrollLeft >= maxScrollLeft ? 'none' : 'flex'
+    }
+
+    window.addEventListener('resize', updateSlider)
 
     images.forEach(imageSrc => {
       const image = document.createElement('img')
@@ -13,38 +52,8 @@ const initSlider = () => {
     const slideButtons = container.querySelectorAll('.slide-button')
     const sliderScrollbar = container.querySelector('.slider-scrollbar')
     const scrollbarThumb = sliderScrollbar.querySelector('.scrollbar-thumb')
-    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth
 
-    scrollbarThumb.addEventListener('mousedown', (e) => {
-      const startX = e.clientX
-      const thumbPosition = scrollbarThumb.offsetLeft
-      const maxThumbPosition =
-            sliderScrollbar.getBoundingClientRect().width -
-            scrollbarThumb.offsetWidth
-
-      const handleMouseMove = (e) => {
-        const deltaX = e.clientX - startX
-        const newThumbPosition = thumbPosition + deltaX
-        const boundedPosition = Math.max(
-          0,
-          Math.min(maxThumbPosition, newThumbPosition)
-        )
-        const scrollPosition =
-            (boundedPosition / maxThumbPosition) * maxScrollLeft
-
-        scrollbarThumb.style.left = `${boundedPosition}px`
-        imageList.scrollLeft = scrollPosition
-      }
-
-      const handleMouseUp = () => {
-        console.log('mouseup-event')
-        document.removeEventListener('mousemove', handleMouseMove)
-        document.removeEventListener('mouseup', handleMouseUp)
-      }
-
-      document.addEventListener('mousemove', handleMouseMove)
-      document.addEventListener('mouseup', handleMouseUp)
-    })
+    updateSlider()
 
     slideButtons.forEach((button) => {
       button.addEventListener('click', () => {
@@ -54,30 +63,44 @@ const initSlider = () => {
       })
     })
 
-    const handleSlideButtons = () => {
-      slideButtons[0].style.display = imageList.scrollLeft <= 0 ? 'none' : 'flex'
-      slideButtons[1].style.display =
-        imageList.scrollLeft >= maxScrollLeft ? 'none' : 'flex'
-    }
+    scrollbarThumb.addEventListener('mousedown', (e) => {
+      const startX = e.clientX
+      const thumbPosition = scrollbarThumb.offsetLeft
+      const maxThumbPosition = sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth
 
-    const updateScrollThumbPosition = () => {
-      const scrollPosition = imageList.scrollLeft
-      const thumbPosition =
-        (scrollPosition / maxScrollLeft) *
-        (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth)
-      scrollbarThumb.style.left = `${thumbPosition}px`
-    }
+      const handleMouseMove = (e) => {
+        const deltaX = e.clientX - startX
+        const newThumbPosition = thumbPosition + deltaX
+        const boundedPosition = Math.max(
+          0,
+          Math.min(maxThumbPosition, newThumbPosition)
+        )
+        const scrollPosition =
+          (boundedPosition / maxThumbPosition) * maxScrollLeft
 
-    imageList.addEventListener('scroll', () => {
-      updateScrollThumbPosition()
-      handleSlideButtons()
+        scrollbarThumb.style.left = `${boundedPosition}px`
+        imageList.scrollLeft = scrollPosition
+      }
+
+      const handleMouseUp = () => {
+        document.removeEventListener('mousemove', handleMouseMove)
+        document.removeEventListener('mouseup', handleMouseUp)
+      }
+
+      document.addEventListener('mousemove', handleMouseMove)
+      document.addEventListener('mouseup', handleMouseUp)
     })
+
+    imageList.addEventListener('scroll', handleScroll)
+
+    return destroyCarousel
   }
+
   const slider1Images = ['res/1.jpg', 'res/2.jpg', 'res/3.jpg', 'res/4.jpg', 'res/5.jpg', 'res/1.jpg']
-  initCarousel('slider1', slider1Images)
+  const destroySlider1 = initCarousel('slider1', slider1Images)
 
   const slider2Images = ['res/6.jpg', 'res/7.jpg', 'res/8.jpg', 'res/9.jpg', 'res/10.jpg', 'res/9.jpg']
-  initCarousel('slider2', slider2Images)
+  const destroySlider2 = initCarousel('slider2', slider2Images)
 }
 
 function scrollToBottom () {
@@ -87,9 +110,4 @@ function scrollToBottom () {
   })
 }
 
-let isListenerAdded = false
-
-window.addEventListener('resize', () => {
-  isListenerAdded = false
-})
 window.addEventListener('load', initSlider)
